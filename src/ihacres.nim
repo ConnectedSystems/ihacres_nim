9a2ed64c4d925fc3665cf4e3f48e001341fc6e9a
ConnectedSystems <takuyai@gmail.com>
2020-08-17 15:30:01 +1000

    Initial commit of node types (not working)

--- src/ihacres.nim
+++ src/ihacres.nim
@@ -1,6 +1,7 @@
 import ihacres/climate
 import ihacres/cmd
 import ihacres/flow
+import ihacres/node
 
 # include ihacres/climate, ihacres/cmd, ihacres/flow
-export climate, cmd, flow
+export climate, cmd, flow, node
