06d2f21f0f32a3fb0e8d4bac96390e4cf3cf6876
ConnectedSystems <takuyai@gmail.com>
2020-08-17 19:37:39 +1000

    separate example node (for dev)

---
+++ tests/example.nim
@@ -0,0 +1,10 @@
+import ihacres/node
+
+var test_node* = StreamNode(
+    area:100, 
+    d: 0.1, d2: 0.1, e: 0.1, f: 0.1, 
+    a: 54.35,
+    b: 0.012,
+    storage_coef: 2.5,
+    storage: @[100.0]
+)
